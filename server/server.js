09de2fcc306e223bf963301de18a7218082c6ca1
Xtremezero <xtremezero@proton.me>
2024-07-30 17:51:29 +0400

    GLORY TO MANKIND

--- server/server.js
+++ server/server.js
@@ -11,58 +11,33 @@ app.get('/run/:program', (req, res) => {
   const lnkPath = path.join('C:/Users/XtremeZero/Documents/shortcuts', `${programName}.lnk`);
   const urlPath = path.join('C:/Users/XtremeZero/Documents/shortcuts', `${programName}.url`);
 
+  const startProgram = (filePath) => {
+    const child = spawn('cmd', ['/c', filePath], {
+      detached: true,
+      stdio: 'ignore',
+      shell: false
+    });
+
+    child.unref();
+
+    child.on('error', (error) => {
+      console.error(`Error executing ${filePath}:`, error);
+      res.status(500).send(`Error running program: ${error.message}`);
+    });
+
+    res.send(`Program ${programName} started successfully.`);
+  };
+
   fs.access(lnkPath, fs.constants.F_OK, (err) => {
     if (!err) {
       // .lnk file exists, spawn a new process to execute it
-      const child = spawn('cmd', ['/c', lnkPath], { shell: true });
-
-      child.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
-
-      child.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-
-      child.on('close', (code) => {
-        if (code !== 0) {
-          res.status(500).send(`Error running program: exited with code ${code}`);
-        } else {
-          res.send(`Program ${programName} started successfully.`);
-        }
-      });
-
-      child.on('error', (error) => {
-        console.error(`Error executing ${lnkPath}:`, error);
-        res.status(500).send(`Error running program: ${error.message}`);
-      });
+      startProgram(lnkPath);
     } else {
       // .lnk file does not exist, check for .url file
       fs.access(urlPath, fs.constants.F_OK, (err) => {
         if (!err) {
           // .url file exists, spawn a new process to open it
-          const child = spawn('cmd', ['/c', urlPath], { shell: true });
-
-          child.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-          });
-
-          child.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-          });
-
-          child.on('close', (code) => {
-            if (code !== 0) {
-              res.status(500).send(`Error running program: exited with code ${code}`);
-            } else {
-              res.send(`Program ${programName} started successfully.`);
-            }
-          });
-
-          child.on('error', (error) => {
-            console.error(`Error executing ${urlPath}:`, error);
-            res.status(500).send(`Error running program: ${error.message}`);
-          });
+          startProgram(urlPath);
         } else {
           // Neither .lnk nor .url file exists
           res.status(404).send(`Program ${programName} not found.`);
